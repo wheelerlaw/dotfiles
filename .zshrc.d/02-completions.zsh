c3b2cd1b8a07acfa5bab73fe9c16a982f280d670
Wheeler Law <3081566+wheelerlaw@users.noreply.github.com>
2026-04-24 12:44:09 -0500

    feat: add docker zsh completions

--- .zshrc.d/02-completions.zsh
+++ .zshrc.d/02-completions.zsh
@@ -1,9 +1,13 @@
 #!/usr/bin/env zsh
 
-if (( $+commands[atlantis] )); then
+if (( $+commands[atlantis] )) && ! (( $+_comps[atlantis] )); then
   eval "$(atlantis completion zsh)"
 fi
 
-if command -v ngrok &>/dev/null; then
-  eval "$(ngrok completion)"
+if (( $+commands[ngrok] )) && ! (( $+_comps[ngrok] )); then
+  eval "$(ngrok completion zsh)"
+fi
+
+if (( $+commands[docker] )) && ! (( $+_comps[docker] )); then
+  eval "$(docker completion zsh)"
 fi
